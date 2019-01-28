4b912ddc605d8bd09a6dbd9d8a25d10b78762067
Wojciech Burczyk <wburczyk@it-dev.pl>
2019-01-28 17:46:19 +0100

    - added index.html - added index.js - added style.css

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Babel from 'babel';
+import 'css/style.css';
+
+ReactDOM.render(
+    <p>Test</p>,
+    document.getElementById('app')
+);
