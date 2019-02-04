4d78d92050629a9bae6f28f335bfb2f7048dd2bb
Wojciech Burczyk <wburczyk@it-dev.pl>
2019-02-04 20:31:54 +0100

    export component to other file.

--- repo-search-react/src/App.js
+++ repo-search-react/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import { Router, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
+//import { Router, browserHistory } from 'react-router';
+//import { Provider } from 'react-redux';
 import axios from 'axios';
-import routes from './routes';
-import configureStore from './configureStore';
-import { loadResults } from './reposAction';
+//import { loadResults } from './reposAction';
 import { RenderSearchBox } from './RenderSearchBox';
 import { SearchResults } from './SeachResults';
 import './App.css';
@@ -14,14 +12,9 @@ export class App extends Component {
     return (
       <section class="main-content">
         <div class="search-container">
-            {/* <RenderSearchBox /> */}
-            <div id="search-box" class="search-header">
-                    <a href="http://github.com/"><img id="logogh" alt="logo-github" src="logogh.png" /></a>
-                    <form method="get">
-                    <input id="search-input" type="text" name="search" placeholder="Search or jump to..."></input>
-                    <input id="search-submit" type="submit" name="submit-phrase" onClick={submitSearch} value="Search"></input>                
-                    </form>
-                </div>
+
+            <RenderSearchBox />
+            
             {/* <SearchResults /> */}
             <div class="result-container">
                 <div class="result-info">
@@ -41,33 +34,38 @@ export class App extends Component {
 export function submitSearch(e) {
   e.preventDefault();
   console.log('Searching for repositories...');
+
+  let inputSearch = document.getElementById("search-input");
+  let inputValue = inputSearch.value();
+
+  // return function loadResults() {
+  //   return function(dispatch) {
+  //     return axios
+  //       .get("https://api.github.com/search/repositories?q=" + inputValue + "&page=1&per_page=10")
+  //       .then(repos => {
+  //         console.log("dispatch(loadResultsSucces(repos.data));")
+  //       })
+  //       .catch(err => {
+  //         throw err;
+  //       });
+  //   }
+  // }
 }
-let inputSearch = document.getElementById("search-input");
-let inputValue = inputSearch.value();
 
-export function loadResults() {
-  return function(dispatch) {
-    return axios
-      .get("https://api.github.com/search/repositories?q=" + inputValue + "&page=1&per_page=10")
-      .then(repos => {
-        dispatch(loadResultsSucces(repos.data));
-      })
-      .catch(err => {
-        throw err;
-      });
-  }
-}
+//
 
-let currValue;
-export function onChange() {
-    let prevValue = currValue;
-    currValue = store.getState().user;
 
-    if(prevValue != currValue) {
-        store.dispatch(loadRepos(repos.data));
+
+// let currValue;
+// export function onChange() {
+//     let prevValue = currValue;
+//     currValue = store.getState().user;
+
+//     if(prevValue != currValue) {
+//         store.dispatch(loadRepos(repos.data));
         
-    }
-}  
+//     }
+// }  
 
 
 export default App;
