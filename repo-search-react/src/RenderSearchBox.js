4b45d1e1cbd5170849c56140682c2b2e50d0b9d9
Wojciech Burczyk <wburczyk@it-dev.pl>
2019-02-04 20:32:26 +0100

    - export RenderSearchBox component

---
+++ repo-search-react/src/RenderSearchBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+ export function RenderSearchBox() {
+        return (
+            <div id="search-box" class="searcher-header">
+                    <a href="http://github.com/"><img id="logogh" alt="logo-github" src="logogh.png" /></a>
+                    <form method="get">
+                    <input id="search-input" type="text" name="search" placeholder="Search or jump to..."></input>
+                    <input id="search-submit" type="submit" name="submit-phrase" onClick="{submitSearch}" value=""></input>                
+                    </form>
+                </div>
+        );
+    }    
+
+
