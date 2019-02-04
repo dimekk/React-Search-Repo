947fc0b8d70d81cb25d845eb85a2200559b81aeb
Wojciech Burczyk <wburczyk@it-dev.pl>
2019-02-04 20:34:35 +0100

    export SearchResults component

--- repo-search-react/src/App.js
+++ repo-search-react/src/App.js
@@ -14,18 +14,8 @@ export class App extends Component {
         <div class="search-container">
 
             <RenderSearchBox />
-            
-            {/* <SearchResults /> */}
-            <div class="result-container">
-                <div class="result-info">
-                    <p id="result-avatar">Author:</p>
-                    <p id="result-name">Repository name:</p>
-                    <p id="result-stars">Stars:</p>
-                </div>
-                <div class="searchResults">
-                   
-                </div>
-            </div>
+
+            <SearchResults />
         </div>
     </section>     
     );
---
+++ repo-search-react/src/SeachResults.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export function SearchResults()  {
+        return(
+            <div class="result-container">
+                <div class="result-info">
+                    <p id="result-avatar">Author:</p>
+                    <p id="result-name">Repository name:</p>
+                    <p id="result-stars">Stars:</p>
+                </div>
+                <div class="searchResults">
+                    
+                </div>
+            </div>
+        )
+    }
+    
