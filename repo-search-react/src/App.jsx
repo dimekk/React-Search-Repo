d38268ddbbe9fa04fae9f8b534bc0c86c3738dad
Wojciech Burczyk <wburczyk@it-dev.pl>
2019-02-10 14:40:10 +0100

    change to jsx

---
+++ repo-search-react/src/App.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { getFilteredRepos } from './selectors/getFilteredRepos';
+
+export class App extends Component {  
+  render() {    
+    return (
+      <section class="main-content">
+        <div class="search-container">
+
+            
+        </div>
+    </section>     
+    );
+  }; 
+}
+
+
+export default App;
